198a6d9b114f06ec78af945daed767a014993706
Michael Hancock <mikehancock3@gmail.com>
2013-08-28 15:16:20 +0100

    fixing assemblyinfo details to match project / assembly names

--- src/MvcTables/Properties/AssemblyInfo.cs
+++ src/MvcTables/Properties/AssemblyInfo.cs
@@ -10,11 +10,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("MvcTable")]
+[assembly: AssemblyTitle("MvcTables")]
 [assembly: AssemblyDescription("An AJAX/HTML Table framework for ASP.Net MVC")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Mark Tranter")]
-[assembly: AssemblyProduct("MvcTable")]
+[assembly: AssemblyProduct("MvcTables")]
 [assembly: AssemblyCopyright("Copyright © Mark Tranter 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
